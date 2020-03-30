e838e64e967dfacc46becf506dde1a74629d2677
André Walker <andre.walkermo@gmail.com>
2020-03-30 02:25:22 -0300

    Mobile da Aplicação

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -13,13 +13,14 @@ module.exports = {
         .offset((page - 1) * 5)
             .select([
                 'incidents.*',
+                'ongs.name',
                 'ongs.email',
                 'ongs.whatsapp',
                 'ongs.city',
                 'ongs.uf'
             ])
         response.header("X-Total-Count", count['count(*)']);
-
+        console.log(incidents);
         return response.json(incidents)
     },
     async create(request, response) {
